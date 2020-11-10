c30e4ab5d1580d147d69025ee4e2888719163709
GeekVan <lychallengers@gmail.com>
2020-11-10 23:40:38 +0800

    需要修改bug :JSON keys must be strings.

--- src/main/java/com/neu/ParseJson.java
+++ src/main/java/com/neu/ParseJson.java
@@ -42,6 +42,49 @@ public class ParseJson {
            return null;
        }
    }
+
+    public static String String2Json(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.toCharArray()[i];
+            switch (c) {
+//                case '\"':
+//                    sb.append("\\\"");
+//                    break;
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '/':
+                    sb.append("\\/");
+                    break;
+                case '\b':
+                    sb.append("\\b");
+                    break;
+                case '\f':
+                    sb.append("\\f");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                default:
+                    if ((c >= 0 && c <= 31) || c == 127)// 在ASCⅡ码中，第0～31号及第127号(共33个)是控制字符或通讯专用字符
+                    {
+
+                    } else {
+                        sb.append(c);
+                    }
+                    break;
+            }
+        }
+        return sb.toString();
+    }
+
    public void parseJson(Connection con,String fileName) throws IOException {
        String insertDblpSql = "insert into dblp_tbl  values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
        String insertAuthorSql = "insert into author_tbl values(?,?,?)";//数据库操作语句（插入）
@@ -68,7 +111,11 @@ public class ParseJson {
        String venue_raw;
        String venue_id;
        String venue_type;
+       int i=0;
        while((readStr=bufferedReader.readLine())!=null){
+           System.out.println(i);
+           i++;
+//           if(i!=1)continue;
            String str=readStr.substring(1);
            JSONObject jb = JSONObject.fromObject(str);
            String  id = jb.getString("id");
@@ -80,7 +127,6 @@ public class ParseJson {
            author_id=getObjectToString(author,"id");
            title=getObjectToString(jb,"title");
            year=getObjectToString(jb,"year");
-           System.out.println(title);
            n_citation=getObjectToString(jb,"n_citation");
            page_start=getObjectToString(jb,"page_start");
            page_end=getObjectToString(jb,"page_end");
@@ -97,7 +143,7 @@ public class ParseJson {
            venue_raw=getObjectToString(venue,"raw");
            venue_id=getObjectToString(venue,"id");
            venue_type=getObjectToString(venue,"type");
-           System.out.println(id);
+//           System.out.println(id);
        }
 
    }
